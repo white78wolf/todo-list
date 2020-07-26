8e26c7d4490fe8f95225db73ec7fde280f55b65d
Andrey Tsepkov <cepkov78@mail.ru>
2020-07-26 19:27:09 +0600

    Add to Todos script section with an array of todos

--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -1,7 +1,34 @@
 <template>
     <div>
         <h1>Todo App - Todos</h1>
-        <hr>
         <router-link to="/">Home</router-link>
+        <hr>
+        <ul
+            v-for="(todo, idx) in todos"
+            :key="todo.id"
+            :index="idx"
+        >
+            <li><strong>{{todo.id}}</strong>. {{todo.title}}</li>
+        </ul>
     </div>
 </template>
+
+<script>
+    export default {
+        data () {
+            return {
+                todos: [
+                    { id: 1, title: 'Learn Vue.js', completed: false },
+                    { id: 2, title: 'Think of your own project', completed: false },
+                    { id: 3, title: 'Do it', completed: false }
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    ul {
+        list-style: none;
+    }
+</style>
