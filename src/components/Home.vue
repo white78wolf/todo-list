dd4d99a355f8dd2d0862e52dd81284627f9526c0
Andrey Tsepkov <cepkov78@mail.ru>
2020-07-26 10:21:49 +0600

    Add link to Todos into Home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,8 @@
 <template>
-    <h1>Todo App - Home</h1>
+    <div>
+        <h1>Todo App - Home</h1>
+        <hr>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque aliquid quod eaque veniam possimus amet magni deleniti, corrupti excepturi nostrum perspiciatis sit et eveniet quos molestias tenetur ipsum iusto odit?</p>
+        <a href="/todos">Todos</a>
+    </div>
 </template>
