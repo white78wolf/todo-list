9852001f6946db554ed0d8db3b2af7476c0dcc26
Andrey Tsepkov <cepkov78@mail.ru>
2020-07-26 19:25:12 +0600

    Edit Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <h1>Todo App - Home</h1>
-        <hr>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque aliquid quod eaque veniam possimus amet magni deleniti, corrupti excepturi nostrum perspiciatis sit et eveniet quos molestias tenetur ipsum iusto odit?</p>
+        <hr>
         <router-link to="/todos">Todos</router-link>
     </div>
 </template>
