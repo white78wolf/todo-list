7c642957a3dcb1b23620af6031aa36a81b34efc6
Andrey Tsepkov <cepkov78@mail.ru>
2020-07-27 16:24:23 +0600

    Transfer list of todos from view to components

---
+++ src/components/TodoList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <ul v-for="(todo, idx) in todos" :key="todo.id" :index="idx">
+      <li><strong>{{todo.id}}</strong>. {{todo.title}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        todos: [{
+            id: 1,
+            title: 'Learn Vue.js',
+            completed: false
+          },
+          {
+            id: 2,
+            title: 'Think of your own project',
+            completed: false
+          },
+          {
+            id: 3,
+            title: 'Do it',
+            completed: false
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  ul {
+    list-style: none;
+  }
+</style>
--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -3,32 +3,16 @@
         <h1>Todo App - Todos</h1>
         <router-link to="/">Home</router-link>
         <hr>
-        <ul
-            v-for="(todo, idx) in todos"
-            :key="todo.id"
-            :index="idx"
-        >
-            <li><strong>{{todo.id}}</strong>. {{todo.title}}</li>
-        </ul>
+        <TodoList />
     </div>
 </template>
 
 <script>
-    export default {
-        data () {
-            return {
-                todos: [
-                    { id: 1, title: 'Learn Vue.js', completed: false },
-                    { id: 2, title: 'Think of your own project', completed: false },
-                    { id: 3, title: 'Do it', completed: false }
-                ]
-            }
-        }
-    }
-</script>
+import TodoList from '@/components/TodoList'
 
-<style scoped>
-    ul {
-        list-style: none;
+export default {
+    components: {
+        TodoList
     }
-</style>
+}
+</script>
