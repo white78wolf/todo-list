92f58d9c7a5f1c92259aaf9938dde5755268bfe8
Andrey Tsepkov <cepkov78@mail.ru>
2020-07-27 20:52:06 +0600

    Create AddTodo component for adding new item

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,29 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <input type="text" v-model="title">
+    <button type="submit">Add new task</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: ""
+    }
+  },
+  methods: {
+    onSubmit () {
+      if (this.title.trim()) {
+        const newTodo = {
+          id: Date.now(),
+          title: this.title.trim(),
+          completed: false
+        }
+        this.$emit('add-todo', newTodo)
+        this.title = ""
+      }
+    }
+  }
+}
+</script>
