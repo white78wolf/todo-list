463696c5cb4f4cd6c19e1814c63aaf1e1dbab4a6
Andrey Tsepkov <cepkov78@mail.ru>
2020-07-27 16:25:45 +0600

    Fix indentations of 4 spaces to 2 spaces

--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -1,18 +1,18 @@
 <template>
-    <div>
-        <h1>Todo App - Todos</h1>
-        <router-link to="/">Home</router-link>
-        <hr>
-        <TodoList />
-    </div>
+  <div>
+    <h1>Todo App - Todos</h1>
+    <router-link to="/">Home</router-link>
+    <hr>
+    <TodoList />
+  </div>
 </template>
 
 <script>
-import TodoList from '@/components/TodoList'
+  import TodoList from '@/components/TodoList'
 
-export default {
+  export default {
     components: {
-        TodoList
+      TodoList
     }
-}
+  }
 </script>
