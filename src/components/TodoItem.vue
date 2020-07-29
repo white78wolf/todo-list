24de2677e229c2d108dd6d5f410fd768a9845c68
Andrey Tsepkov <cepkov78@mail.ru>
2020-07-29 21:20:09 +0600

    Add material design icons

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,18 +1,30 @@
 <template>
   <li>
-    <span :class="{ completed: todo.completed }">
-      <input
-        type="checkbox"
-        :checked="todo.completed"
-        @change="todo.completed = !todo.completed"
-      >
-      {{todo.title}}
+    <span :class="{ completed: todo.completed }" class="checkbox-and-title">
+      <CheckboxBlankOutlineIcon
+        v-if="!todo.completed"
+        @click="todo.completed = !todo.completed"
+        class="checkbox"
+      />
+      <CheckboxMarkedOutline
+        v-else
+        @click="todo.completed = !todo.completed"
+        class="checkbox"
+      />
+      <span class="todo-title">{{todo.title}}</span>
     </span>
-    <button @click="$emit('remove-item', todo.id)">&times;</button>
+    <button
+      @click="$emit('remove-item', todo.id)"
+      class="delete-button"
+    ><delete-icon title="Delete the task"/></button>
   </li>
 </template>
 
 <script>
+import DeleteIcon from 'vue-material-design-icons/Delete.vue'
+import CheckboxBlankOutlineIcon from 'vue-material-design-icons/CheckboxBlankOutline.vue'
+import CheckboxMarkedOutline from 'vue-material-design-icons/CheckboxMarkedOutline.vue'
+
 export default {
   props: {
     todo: {
@@ -20,6 +32,11 @@ export default {
       required: true
     },
     index: Number
+  },
+  components: {
+    DeleteIcon,
+    CheckboxBlankOutlineIcon,
+    CheckboxMarkedOutline
   }
 }
 </script>
@@ -35,8 +52,28 @@ li {
 input {
   margin-right: 1rem;
 }
+.checkbox-and-title {
+  display: flex;
+  justify-content: start;
+  align-items: center;
+}
+.checkbox {
+  margin-right: 1rem;
+  color: #444;
+  cursor: pointer;
+}
+.checkbox:active {
+  border: 2px solid #444;
+  border-radius: 5px;
+}
 .completed {
   text-decoration: line-through;
   color: #777;
+}
+.delete-button {
+  border: none;
+  background-color: #fff;
+  color: #444;
+  cursor: pointer;
 }
 </style>
