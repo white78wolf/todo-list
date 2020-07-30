0a54044cdafce5ce23484cc9273ced2f8d8c846e
Andrey Tsepkov <cepkov78@mail.ru>
2020-07-30 17:24:15 +0600

    Wrap up the anchor of Home page into a button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,29 @@
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque aliquid quod eaque veniam possimus amet magni
       deleniti, corrupti excepturi nostrum perspiciatis sit et eveniet quos molestias tenetur ipsum iusto odit?</p>
     <hr>
-    <router-link to="/todos">Todos</router-link>
+    <button class="anchor-button">
+      <router-link to="/todos">Todos</router-link>
+    </button>
   </div>
 </template>
+
+<style scoped>
+a {
+  text-decoration: none;
+}
+a:visited {
+  color: #444;
+}
+.anchor-button {
+  font-size: 1.5rem;
+  border: none;
+  border-radius: 10px;
+  background: #eee;
+  width: 7rem;
+  height: 3rem;
+}
+.anchor-button:hover {
+  cursor: pointer;
+  background: #ccc;
+}
+</style>
